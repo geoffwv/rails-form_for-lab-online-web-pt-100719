ce8ec9cbb8bcd086d508ce5a2df1be7f5c8e1bf5
github username <github email address>
2020-03-10 23:20:21 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :school_classes, only: [:show, :new, :edit, :create, :update]
+  patch 'school_class/:id', to: 'school_class#update'
+
+  resources :students, only: [:show, :new, :edit, :create, :update]
+  patch 'student/:id', to: 'student#update'
 end
---
+++ db/migrate/20200311002429_create_school_classes.rb
@@ -0,0 +1,12 @@
+class CreateSchoolClasses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :school_classes do |t|
+      t.string :title
+      t.string :string
+      t.string :room_number
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
